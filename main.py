ab585770ff8502a97005170f4c9c42a2506e0486
Jay <the7mincheol@gmail.com>
2016-07-30 22:15:11 +0900

    finished chapter2

---
+++ main.py
@@ -0,0 +1,25 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ObjectProperty
+from kivy.network.urlrequest import UrlRequest
+import json
+
+class AddLocationForm(BoxLayout):
+    search_input = ObjectProperty()
+    search_results = ObjectProperty()
+    
+    def search_location(self):
+        search_template = "http://api.openweathermap.org/data/2.5/find?q={}&type=like&APPID=" + "31584e338edb0ea301c9f00df1b046d7"
+        search_url = search_template.format(self.search_input.text)
+        request = UrlRequest(search_url, self.found_location)
+
+    def found_location(self, request, data):
+        data = json.loads(data.decode()) if not isinstance(data, dict) else data
+        cities = ["{} ({})".format(d['name'], d['sys']['country']) for d in data['list']]
+        self.search_results.item_strings = cities
+
+class WeatherApp(App):
+    pass
+
+if __name__ == '__main__':
+    WeatherApp().run()
