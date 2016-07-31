2a2343140ec7abea597477c36842bd98a8db973b
Jay <the7mincheol@gmail.com>
2016-07-31 22:29:34 +0900

    p40

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import ObjectProperty
 from kivy.network.urlrequest import UrlRequest
 import json
+from kivy.uix.listview import ListItemButton
+
+class LocationButton(ListItemButton):
+    pass
+
+class WeatherRoot(BoxLayout):
+    def show_current_weather(self, location):
+        from kivy.uix.label import Label
+        self.clear_widgets()
+        self.add_widget(Label(text=location))
 
 class AddLocationForm(BoxLayout):
     search_input = ObjectProperty()
@@ -17,6 +27,9 @@ class AddLocationForm(BoxLayout):
         data = json.loads(data.decode()) if not isinstance(data, dict) else data
         cities = ["{} ({})".format(d['name'], d['sys']['country']) for d in data['list']]
         self.search_results.item_strings = cities
+        self.search_results.adapter.data.clear()
+        self.search_results.adapter.data.extend(cities)
+        self.search_results._trigger_reset_populate()
 
 class WeatherApp(App):
     pass
